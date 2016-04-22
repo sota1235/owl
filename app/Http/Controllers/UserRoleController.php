f7660acc251e261ddd223149379b9df5e845eb54
sota1235 <sota1235@gmail.com>
2016-04-22 16:10:13 +0200

    modify: change for new auth logic

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -1,55 +1,84 @@
 <?php namespace Owl\Http\Controllers;
 
+/**
+ * @copyright (c) owl
+ */
+
+use Illuminate\Auth\AuthManager;
 use Owl\Services\UserService;
-use Owl\Services\AuthService;
 use Owl\Services\UserRoleService;
 
+/**
+ * Class UserRoleController
+ */
 class UserRoleController extends Controller
 {
+    /** @var AuthManager */
+    protected $auth;
+
+    /** @var UserService */
     protected $userService;
-    protected $authService;
+
+    /** @var UserRoleService */
     protected $userRoleService;
 
+    /**
+     * @param AuthManager      $auth
+     * @param UserService      $userService
+     * @param UserRoleService  $userRoleService
+     */
     public function __construct(
-        UserService $userService,
-        AuthService $authService,
+        AuthManager     $auth,
+        UserService     $userService,
         UserRoleService $userRoleService
     ) {
-        $this->userService = $userService;
-        $this->authService = $authService;
+        $this->auth            = $auth;
+        $this->userService     = $userService;
         $this->userRoleService = $userRoleService;
     }
 
+    /**
+     * オーナー初期登録画面
+     *
+     * @return \Illuminate\View\View
+     * @throw \Exception
+     */
     public function initial()
     {
         $owners = $this->userService->getOwners();
-        if (! empty($owners)) {
-            \App::abort(500);
+        if (!empty($owners)) {
+            abort(500);
         }
 
-        $user = $this->userService->getCurrentUser();
-        if ($user == null) {
-            \App::abort(404);
+        $user = $this->auth->user();
+        if (is_null($user)) {
+            abort(404);
         }
 
         return view('user.role.initial', compact('user'));
     }
 
+    /**
+     * オーナー登録処理
+     *
+     * @return \Illuminate\View\View
+     * @throw \Exception
+     */
     public function initialRegister()
     {
         $owners = $this->userService->getOwners();
-        if (! empty($owners)) {
-            \App::abort(500);
+        if (!empty($owners)) {
+            abort(500);
         }
 
-        $user = $this->userService->getCurrentUser();
-        if ($user == null) {
-            \App::abort(404);
+        $user = $this->auth->user();
+        if (is_null($user)) {
+            abort(404);
         }
 
         $user->role = UserRoleService::ROLE_ID_OWNER;
-        $updateUser = $this->userService->update($user->id, $user->username, $user->email, $user->role);
-        $this->authService->setUser($updateUser);
+        $updateUser = $this->userService->update($user->getAuthIdentifier(), $user->name(), $user->email(), $user->role());
+        $this->auth->login($updateUser);
 
         return view('user.role.initialComplete');
     }
