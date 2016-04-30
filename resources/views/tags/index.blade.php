4e7dbe91f74f8c6ffc8fb0956c5c8bec15256cbd
sota1235 <sota1235@gmail.com>
2016-04-30 06:53:41 +0200

    modify: replace path to route helper on tag list page

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -23,8 +23,8 @@
         @foreach ($tags as $tag)
         <li class="list-group-item">
             <span class="badge">{{ $tag->count }}</span>
-            <p class="list-group-item-heading"><span class="tag-label"><a href="/tags/{{ $tag->name }}">{{ $tag->name }}</a></span></p>
-            <p class="list-group-item-text">　<a href="/items/{{ $tag->open_item_id }}">{{ $tag->title }}</a></p>
+            <p class="list-group-item-heading"><span class="tag-label"><a href="{{{ route('tags.show', ['tags' => $tag->name]) }}}">{{ $tag->name }}</a></span></p>
+            <p class="list-group-item-text">　<a href="{{{ route('items.show', ['items' => $tag->open_item_id]) }}}">{{ $tag->title }}</a></p>
         </li>
         @endforeach
     </ul>
@@ -38,7 +38,7 @@
         <div class="sidebar-info-items">
             <ol>
             @for ($i = 0; $i < count($recent_ranking); $i++)
-                <li><a href="{{ action('ItemController@show', $recent_ranking[$i]->open_item_id) }}">{{{ $recent_ranking[$i] -> title }}}</a></li>
+                <li><a href="{{{ route('items.show', ['items' => $recent_ranking[$i]->open_item_id]) }}}">{{{ $recent_ranking[$i] -> title }}}</a></li>
             @endfor
             </ol>
         </div>
@@ -49,7 +49,7 @@
         <div class="sidebar-info-items">
             <ol>
             @for ($i = 0; $i < count($all_ranking); $i++)
-                <li><a href="{{ action('ItemController@show', $all_ranking[$i]->open_item_id) }}">{{{ $all_ranking[$i] -> title }}}</a></li>
+                <li><a href="{{{ route('items.show', ['items' => $all_ranking[$i]->open_item_id]) }}}">{{{ $all_ranking[$i] -> title }}}</a></li>
             @endfor
             </ol>
         </div>
