6c856e8103531311870000da87bb375b4bbfef22
sota1235 <sota1235@gmail.com>
2016-04-30 06:24:01 +0200

    modify: replace path to route helper on navbar-menu blade component

--- resources/views/layouts/navbar-menu.blade.php
+++ resources/views/layouts/navbar-menu.blade.php
@@ -1,26 +1,26 @@
 @section('navbar-menu')
 @if(isset($User))
 <ul class="nav navbar-nav navbar-right">
-    <li><a href="/items/create">投稿する</a></li>
-    <li><a href="/favorites">お気に入り一覧</a></li>
+    <li><a href="{{{ route('items.create') }}}">投稿する</a></li>
+    <li><a href="{{{ route('favorites.index') }}}">お気に入り一覧</a></li>
 
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">{!! HTML::gravator($User->email, 20) !!}<span class="caret"></span></a>
         <ul class="dropdown-menu" role="menu">
-            <li><a href="/{{{$User->username}}}">マイページ</a></li>
-            <li><a href="/user/edit">ユーザ情報変更</a></li>
+            <li><a href="{{{ route('user.profile', ['username' => $User->username]) }}}">マイページ</a></li>
+            <li><a href="{{{ route('user.edit.form') }}}">ユーザ情報変更</a></li>
 @if($User->role == Owl\Services\UserRoleService::ROLE_ID_OWNER)
-            <li><a href="/manage">管理画面</a></li>
+            <li><a href="{{{ route('admin') }}} ">管理画面</a></li>
 @endif
             <li class="divider"></li>
-            <li><a href="/logout">ログアウト</a></li>
+            <li><a href="{{{ route('logout') }}}">ログアウト</a></li>
         </ul>
     </li>
 </ul>
 @else
 <ul class="nav navbar-nav navbar-right">
-    <li><a href="/signup">新規登録</a></li>
-    <li><a href="/login">ログイン</a></li>
+    <li><a href="{{{ route('signup') }}}">新規登録</a></li>
+    <li><a href="{{{ route('login.form') }}}">ログイン</a></li>
 </ul>
 @endif
 @stop
