cf715b2781d8848e491a4af50217fe0a13def7de
sota1235 <sota1235@gmail.com>
2016-04-30 06:31:33 +0200

    modify: replace path to route helper on mange/index blade component

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -16,8 +16,8 @@
     <div class="page-header">
         <h5>管理メニュー一覧</h5>
     </div>
-    <p><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <a href="/manage/user/index">ユーザー管理</a></p>
-    <p><span class="glyphicon glyphicon-tags" aria-hidden="true"></span> <a href="/manage/flow/index">フロータグ管理</a></p>
+    <p><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <a href="{{{ route('user.show') }}}">ユーザー管理</a></p>
+    <p><span class="glyphicon glyphicon-tags" aria-hidden="true"></span> <a href="{{{ route('flow.show') }}}">フロータグ管理</a></p>
 
 @stop
 
